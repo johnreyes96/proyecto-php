d03d7ba668a3f813f51e16066c45c4663c001300
johnfreyes <jhonfer96@hotmail.com>
2019-11-14 20:13:16 -0500

    Se cambia el titulo de la página

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="description" content="">
         <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
         <meta name="generator" content="Jekyll v3.8.5">
-        <title>Sovehi</title>
+        <title>SOVEHI</title>
         <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/blog/">
 
         <!-- Bootstrap core CSS -->
