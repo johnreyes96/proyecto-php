c58b5a7f106cfe01550e0a5db9ac8ee5e6cec7b0
johnfreyes <jhonfer96@hotmail.com>
2019-11-16 14:40:31 -0500

    CRUD - ciudades

---
+++ resources/views/ciudades/index.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.app')
+
+@section('content')
+    <hr>
+    <div class="container">
+        <div class="row">
+            <!--@include('admin.sidebar')-->
+
+            <div class="col-md-9">
+                <div class="card">
+                    <div class="card-header">Ciudades</div>
+                    <div class="card-body">
+                        <a href="{{ url('/ciudades/create') }}" class="btn btn-success btn-sm" title="Add New Ciudade">
+                            <i class="fa fa-plus" aria-hidden="true"></i> Crear ciudad
+                        </a>
+
+                        <form method="GET" action="{{ url('/ciudades') }}" accept-charset="UTF-8" class="form-inline my-2 my-lg-0 float-right" role="search">
+                            <div class="input-group">
+                                <input type="text" class="form-control" name="search" placeholder="Buscar..." value="{{ request('search') }}">
+                                <span class="input-group-append">
+                                    <button class="btn btn-secondary" type="submit">
+                                        <i class="fa fa-search"></i>
+                                    </button>
+                                </span>
+                            </div>
+                        </form>
+
+                        <br/>
+                        <br/>
+                        <div class="table-responsive">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>#</th><th>Ciudad</th><th>Acciones</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($ciudades as $item)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $item->NombreCiudad }}</td>
+                                        <td>
+                                            <a href="{{ url('/ciudades/' . $item->id) }}" title="Ver ciudad"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> Ver</button></a>
+                                            <a href="{{ url('/ciudades/' . $item->id . '/edit') }}" title="Editar Ciudad"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Editar</button></a>
+
+                                            <form method="POST" action="{{ url('/ciudades' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
+                                                {{ method_field('DELETE') }}
+                                                {{ csrf_field() }}
+                                                <button type="submit" class="btn btn-danger btn-sm" title="Borrar Ciudad" onclick="return confirm(&quot;Estás seguro que deseas eliminar la ciudad {{ $item->NombreCiudad }}?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Borrar</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                            <div class="pagination-wrapper"> {!! $ciudades->appends(['search' => Request::get('search')])->render() !!} </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br>
+@endsection
---
+++ resources/views/ciudades/show.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+    <hr>
+    <div class="container">
+        <div class="row">
+            <!--@include('admin.sidebar')-->
+
+            <div class="col-md-9">
+                <div class="card">
+                    <div class="card-header">Ciudad {{ $ciudade->id }}</div>
+                    <div class="card-body">
+
+                        <a href="{{ url('/ciudades') }}" title="Regresar"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Regresar</button></a>
+                        <a href="{{ url('/ciudades/' . $ciudade->id . '/edit') }}" title="Editar Ciudad"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Editar</button></a>
+
+                        <form method="POST" action="{{ url('ciudades' . '/' . $ciudade->id) }}" accept-charset="UTF-8" style="display:inline">
+                            {{ method_field('DELETE') }}
+                            {{ csrf_field() }}
+                            <button type="submit" class="btn btn-danger btn-sm" title="Borrar Ciudad" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Borrar</button>
+                        </form>
+                        <br/>
+                        <br/>
+
+                        <div class="table-responsive">
+                            <table class="table">
+                                <tbody>
+                                    <tr>
+                                        <th>ID</th><td>{{ $ciudade->id }}</td>
+                                    </tr>
+                                    <tr><th> Nombre Ciudad </th><td> {{ $ciudade->NombreCiudad }} </td></tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br>
+@endsection
